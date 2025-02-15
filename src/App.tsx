c5b647cebdde09c9474e307eafe38470b3f33b4d
gideon <kipkerring.gedion@gmail.com>
2025-02-15 09:08:11 +0300

    tailwind and react installed and tested

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+const App = () => {
+  return (
+    <div>
+      <p className="text-2xl font-bold text-red-500 underline">
+        Did My Tailwindcss Install
+      </p>
+    </div>
+  );
+};
+export default App;
