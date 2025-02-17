4e7f2ca49700167dfba83b7e70e2b376bd6207f2
gideon <kipkerring.gedion@gmail.com>
2025-02-17 10:40:52 +0300

    implementing the useTransitionHook on three files contact, home and posts; suppose we have so many posts what will be the effect on transitions this commit illustrates it

--- src/Components/UseTransitionHook/Posts.tsx
+++ src/Components/UseTransitionHook/Posts.tsx
@@ -1,4 +1,17 @@
 const Posts = () => {
-  return <div>Posts</div>;
+  // generating many posts
+  const posts = Array.from(
+    { length: 10000 },
+    (_, index) => `Post ${index + 1}`
+  );
+  return (
+    <div>
+      {posts.map((post) => (
+        <div key={post} className="post">
+          {post}
+        </div>
+      ))}
+    </div>
+  );
 };
 export default Posts;
